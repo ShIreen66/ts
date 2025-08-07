702fc04d044543a7205f88b9ad30054c7f7e301b
Shireen Khan <066shiree@gmail.com>
2025-08-07 11:57:11 +0530

    Primitives = Number String Boolean Any

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("firstmmmmmmmmmmmmmmmmmmmmmmmm");
+// Primitives = Number String Boolean Any
+// let varname:datatype: = value (syntax)
+var age = 34;
+var myName = "Alice";
+var invalid = false;
+var id = "fj12234";
+console.log(age, myName, invalid, id);
--- script.ts
+++ script.ts
@@ -1,1 +1,10 @@
-console.log("first")
+// Primitives = Number String Boolean Any
+// let varname:datatype: = value (syntax)
+
+
+let age: number = 34;
+let myName: string = "Alice";
+let invalid: boolean = false;
+let id: any = "fj12234";
+
+console.log(age, myName, invalid, id)
